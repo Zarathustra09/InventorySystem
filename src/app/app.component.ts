18241fce114854c59d5ded36439b8942f2ee7454
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-06-19 16:34:19 +0800

    Products Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AuthService} from "./Services/auth.service";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {ProductService} from "./Services/product.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [AuthService,ProductService]
 })
 export class AppComponent {
   title = 'InventorySystem';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import {DashboardComponent} from "./Components/dashboard/dashboard.component";
+import {LoginComponent} from "./Components/login/login.component";
+import {RegistrationComponent} from "./Components/registration/registration.component";
+import {ProductsComponent} from "./Components/products/products.component";
+import {TransactionsComponent} from "./Components/transactions/transactions.component";
+import {CreateProductComponent} from "./Components/create-product/create-product.component";
+import {UpdateProductComponent} from "./Components/update-product/update-product.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'transactions', component: TransactionsComponent},
+  {path: 'create-product', component: CreateProductComponent},
+  {path: 'update-product/:id', component: UpdateProductComponent},
+
+];
